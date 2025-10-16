e68c33f136af3cc9e3c487be11deec6158c7affe
VijeshVS <vijeshsshetty@gmail.com>
2025-10-16 22:24:13 +0530

    wc done right way

--- wc-tool/main.cpp
+++ wc-tool/main.cpp
@@ -1,249 +1,118 @@
 #include <iostream>
-#include <fstream>
-#include <cctype>
-#include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-// Abstract classes
-
-class CommandDispatcher {
+class STDINStream {
     public:
-    virtual void dispatch(const vector<string>& args) = 0;
-    virtual ~CommandDispatcher() = default;
-};
+        STDINStream() {}
+        static void processStream(){
+            int charCount = 0;
+            int lineCount = 0;
+            int byteCount = 0;
 
-class CommandExecutor {
-    public:
-    virtual void execute(vector<string> args) = 0;
-    virtual ~CommandExecutor() = default;
-};
-
-// Parses/Utitlity class
-
-class UserInput {
-    public:
-    static string getInput(){
-        string cmd;
-        getline(cin,cmd);
-        return cmd;
-    }
-};
-
-class CommandParser {
-    public:
-    static vector<string> parse(string cmd){
-        vector<string> args;
-        string arg = "";
-        
-        int i = 0;
-        while(i < cmd.size()){
-            if(cmd[i] == ' '){
-                args.push_back(arg);
-                arg = "";
-            }
-            else {
-                arg += cmd[i];
-            }
-            i++;
-        }
-        args.push_back(arg);
-
-        return args;
-    }
-};
-
-// Command Executor : Concrete classes
-
-class ByteCounter : public CommandExecutor {
-    public:
-    void execute(vector<string> args) override {
-
-        ifstream file(args[0]);
-        if (!file.is_open()) {
-            cerr << "[File not found] " << args[0] << endl;
-            return;
-        }
-
-        string line;
-        long long cnt = 0;
-        while (getline(file, line)) {
-            cnt += line.size() + 1;
-        }
-        cout << cnt << " " << args[0] << endl;
-        file.close();
-    }
-};
-
-class LineCounter : public CommandExecutor {
-    public:
-    void execute(vector<string> args) override {
-
-        ifstream file(args[0]);
-        if (!file.is_open()) {
-            cerr << "[File not found] " << args[0] << endl;
-            return;
-        }
-
-        string line;
-        long long cnt = 0;
-        while (getline(file, line)) {
-            cnt++;
-        }
-        cout << cnt << " " << args[0] << endl;
-        file.close();
-    }
-};
-
-class WordCounter : public CommandExecutor {
-    public:
-    int getNoOfWords(string line){
-        int cnt = 0;
-        int i = 0;
-
-        while(i < line.length()){
-            while(i < line.length() && isspace(line[i])) i++;
-            if(i < line.length()) cnt++;
-            while(i < line.length() && !isspace(line[i])) i++;
-        }
-
-        return cnt;
-    }
-
-    void execute(vector<string> args) override {
-        ifstream file(args[0]);
-        if(!file.is_open()){
-            cerr << "[File not found] " << args[0] << endl;
-            return;
-        }
-
-        string line;
-        long long cnt = 0;
-        while(getline(file,line)){
-            cnt += getNoOfWords(line);
-        }
-
-        cout << cnt << " " << args[0] << endl;
-        file.close();
-    }
-};
-
-class CharacterCounter : public CommandExecutor {
-    public:
-    void execute(vector<string> args) override {
-        ifstream file(args[0], ios::binary);
-        if (!file.is_open()) {
-            cerr << "[File not found] " << args[0] << endl;
-            return;
-        }
-
-        long long cnt = 0;
-        char ch;
-        while (file.get(ch)) {
-            cnt++;
-        }
-
-        cout << cnt << " " << args[0] << endl;
-        file.close();
-    }
-};
-
-// Factories
-
-class CommandExecutorFactory {
-    public:
-    static CommandExecutor* getExecutor(const string& flag) {
-        // Byte counter
-        if (flag == "-c") {
-            return new ByteCounter();
-        }
-        // Line counter
-        else if(flag == "-l") {
-            return new LineCounter();
-        }
-        // WordCounter
-        else if(flag == "-w"){
-            return new WordCounter();
-        }
-        // Character Counter
-        else if(flag == "-m"){
-            return new CharacterCounter();
-        }
-        return nullptr;
-    }
-};
-
-
-// Command Dispatcher : Concrete classes
-
-class wcDispatcher : public CommandDispatcher {
-    public:
-    void dispatch(const vector<string>& args) override {
-        if(args.size() < 1){
-            cout << "[Invalid Command] wc" << endl;
-            return;
-        }
-        
-        if(args.size() == 1){
-            // execute -c -w -l
-            CommandExecutor* byteCounter = CommandExecutorFactory::getExecutor("-c");
-            CommandExecutor* wordCounter = CommandExecutorFactory::getExecutor("-w");
-            CommandExecutor* lineCounter = CommandExecutorFactory::getExecutor("-l");
-
-            if(byteCounter && wordCounter && lineCounter){
-                byteCounter->execute(args);
-                wordCounter->execute(args);
-                lineCounter->execute(args);
+            string line;
+            while(getline(cin, line)){
+                charCount += line.length();
+                lineCount++;
+                byteCount += line.length() + 1; // +1 for newline character
             }
 
-            delete byteCounter;
-            delete wordCounter;
-            delete lineCounter;
+            cout << charCount << " " << lineCount << " " << byteCount << endl;
         }
-        else {
-            CommandExecutor* executor = CommandExecutorFactory::getExecutor(args[0]);
-            if (executor) {
-                executor->execute(vector<string>(args.begin()+1, args.end()));
-                delete executor;
-            } else {
-                cout << "[Invalid flag] " << args[0] << endl;
-            }
-        }
-    }
 };
 
-class CommandDispatcherFactory {
+class StreamCount {
     public:
-    static CommandDispatcher* getDispatcher(const string& name){
-        if(name == "wc"){
-            return new wcDispatcher();
-        }
-
-        return nullptr;
-    }
+        int charCount;
+        int lineCount;
+        int byteCount;
+        StreamCount(int c, int l, int b) : charCount(c), lineCount(l), byteCount(b) {}
 };
 
-int main(){
-    string cmd = UserInput::getInput();
+class FileStream {
+    string filename;
+    public:
+        FileStream(const string &filename) : filename(filename) {}
 
-    // split cmd into args
-    vector<string> args = CommandParser::parse(cmd);
+        StreamCount processStream(){
+            ifstream file(filename);
+            if(!file.is_open()){
+                cerr << "Error: Could not open file " << filename << endl;
+                return StreamCount(0, 0, 0);
+            }
 
-    if(args.size() < 2){
-        cout << "[Invalid Command] " << cmd << endl;
-        return 1;
+            int charCount = 0;
+            int lineCount = 0;
+            int byteCount = 0;
+
+            string line;
+            while(getline(file, line)){
+                charCount += line.length();
+                lineCount++;
+                byteCount += line.length() + 1; // +1 for newline character
+            }
+
+            file.close();
+            return StreamCount(charCount, lineCount, byteCount);
+        }
+};
+
+int main(int argc, char *argv[]){
+
+    bool showChars = false, showLines = false, showBytes = false;
+
+    if(argc > 1 && argv[1][0] == '-'){
+        char flag = argv[1][1];
+        if(flag == 'm') showChars = true;
+        else if(flag == 'l') showLines = true;
+        else if(flag == 'c') showBytes = true;
     }
 
-    // get the dispatcher
-    CommandDispatcher *dispatcher = CommandDispatcherFactory::getDispatcher(args[0]);
-    if(dispatcher){
-        dispatcher->dispatch(vector<string>(args.begin() + 1, args.end()));
+    vector<string> files;
+    for(int i = 2;i<argc;i++){
+        files.push_back(argv[i]);
+    }
+
+    if(files.empty()){
+        STDINStream::processStream();
     }
     else {
-        cout << "[Command not found] " << cmd << endl;
-    }
+        StreamCount total(0, 0, 0);
+        for(const string &file : files){
+            FileStream fs(file);
+            StreamCount sc = fs.processStream();
+            total.charCount += sc.charCount;
+            total.lineCount += sc.lineCount;
+            total.byteCount += sc.byteCount;
 
-    delete dispatcher;
-    return 0;
+            if(!showBytes && !showChars && !showLines){
+                cout << sc.charCount << " " << sc.lineCount << " " << sc.byteCount << " " << file << endl;
+            } 
+            else if(showChars){
+                cout << sc.charCount << " " << file << endl;
+            }
+            else if(showLines){
+                cout << sc.lineCount << " " << file << endl;
+            }
+            else if(showBytes){
+                cout << sc.byteCount << " " << file << endl;
+            }
+        }
+        if(files.size() > 1) {
+            if(!showBytes && !showChars && !showLines){
+                cout << total.charCount << " " << total.lineCount << " " << total.byteCount << " total" << endl;
+            } 
+            else if(showChars){
+                cout << total.charCount << " total" << endl;
+            }
+            else if(showLines){
+                cout << total.lineCount << " total" << endl;
+            }
+            else if(showBytes){
+                cout << total.byteCount << " total" << endl;
+            }
+        }
+    }
 }
